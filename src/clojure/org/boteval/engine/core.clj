b92c96202a907a7211d324d571a26a33cbd63be4
matan <dev.matan@gmail.com>
2017-04-08 09:11:49 +0300

    refactor

--- src/clojure/org/boteval/engine/core.clj
+++ src/clojure/org/boteval/engine/core.clj
@@ -1,15 +1,12 @@
 (ns org.boteval.engine.core)
 
-(defn sendToBot [message]
-  (println "sample driver sending message" message))
-
+;; function for obtaining new top-level context
 (defn new-context []
   (def ^:dynamic context []) ; initialize to empty vec
   context
 )
 
-;(def ^:dynamic context []) ; initialize to empty vec
-
+;; the function that runs a scenario
 (defn run-scenario [fn scenario-name context params]
   (let [new-context (conj context scenario-name)]
     (println "running scenario" scenario-name ", context is now: " new-context)
--- test/boteval/core_test.clj
+++ test/boteval/core_test.clj
@@ -1,11 +1,11 @@
 ;; currently runs the clojure samples, not testing the framework's own code
 
 (ns boteval.core-test
-  (:require [clojure.test :refer :all]
-            [org.boteval.engine.core :as engine]
-            [boteval.test-scenarios :as sample-scenarios]))
+  (:require [clojure.test :refer :all])
+  (:use [org.boteval.engine.core]
+        [boteval.test-scenarios]))
 
 (deftest run
   (testing "bundle run"
-    (let [context (engine/new-context)]
-    (engine/run-scenario sample-scenarios/master-scenario1 "master" context []))))
+    (let [context (new-context)]
+    (run-scenario master-scenario1 "master" context []))))
