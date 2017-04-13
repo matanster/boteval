04e082b3647c1f9e2de8e3aa1b1acc3dd95e323c
matan <dev.matan@gmail.com>
2017-04-13 22:14:15 +0300

    add sql and mysql dependencies

--- project.clj
+++ project.clj
@@ -8,8 +8,11 @@
   :java-source-paths ["src/java"]
   :javac-options     ["-target" "1.8" "-source" "1.8"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.aviso/pretty "0.1.33"]]
-  :plugins [[io.aviso/pretty "0.1.33"]] ;pretty exceptions in leinigen and repl
+                 [io.aviso/pretty "0.1.33"] ; pretty exceptions in leinigen
+                 [honeysql "0.8.2"]         ; sort of a query
+                 [mysql/mysql-connector-java "5.1.41"] ; mysql jdbc driver
+                 [hikari-cp "1.7.5"]]       ; jdbc connection pooling, if we'll need it
+  :plugins [[io.aviso/pretty "0.1.33"]]
   :aot [org.boteval.engine.api]
   :profiles {:java-tests-compile
     {:java-source-paths ["src/java-test"]}}
