f843f4be687d03ba15640d0111e6395f97262d61
matan <dev.matan@gmail.com>
2017-04-14 15:34:25 +0300

    minor tidy up

--- project.clj
+++ project.clj
@@ -4,16 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
+
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :javac-options     ["-target" "1.8" "-source" "1.8"]
+
   :dependencies [[org.clojure/clojure "1.8.0"]
+
                  [io.aviso/pretty "0.1.33"] ; pretty exceptions in leinigen
+
                  [clj-time "0.13.0"]        ; https://github.com/clj-time/clj-time
+
+                 ;; sql wrangling stack
                  [honeysql "0.8.2"]         ; sort of a query builder
                  [mysql/mysql-connector-java "5.1.41"]  ; mysql jdbc driver
-                 [org.clojure/java.jdbc "0.7.0-alpha3"] ; clojure jdbc
+                 [org.clojure/java.jdbc "0.7.0-alpha3"] ; clojure jdbc, needed for the rest of them libraries
                  [hikari-cp "1.7.5"]]       ; jdbc connection pooling, if we really need it (https://github.com/tomekw/hikari-cp)
+
   :plugins [[io.aviso/pretty "0.1.33"]]
   :aot [org.boteval.engine.api]
   :profiles {:java-tests-compile
